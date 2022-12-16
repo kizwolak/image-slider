b0fb31d36d97eacc1026a69af44b63053c3ccee4
Krzysztof <ki.zwolak@gmail.com>
2022-12-16 04:41:53 +0100

    Right button logic

--- src/index.js
+++ src/index.js
@@ -51,3 +51,31 @@ leftButton.addEventListener('click', () => {
     }, 6000);
 
 });
+
+rightButton.addEventListener('click', () => {
+    img[i].style.zIndex = '-1';
+    img[i].style.transition = '1s ease';
+    img[i].style.transform = 'translateX(100%)';
+    img[i+1].style.transition = '0s ease';
+    img[i+1].style.transform = 'translateX(-100%)';
+    setTimeout(function () {
+        img[i].style.display = 'none';
+        img[i].style.transform = 'translateX(-100%)';
+        img[i].style.zIndex = '0';
+        img[i+1].style.display = 'block';
+    }, 1000);
+    setTimeout(function () {
+        if (i == (img.length - 1) ) {
+            i = 0;
+            img[i].style.display = 'block';
+            img[i].style.transition = '2s ease';
+            img[i].style.transform = 'translateX(0%)';
+            return;
+        } else {
+            img[i+1].style.transition = '2s ease';
+            img[i+1].style.transform = 'translateX(0%)';
+            return i += 1;
+        }
+    }, 6000);
+
+});
